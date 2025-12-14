0d95da9cdf360348d5e39c703802d8528a6f6d72
Junayed Noman <138345567+junayednoman@users.noreply.github.com>
2025-12-14 18:22:57 +0600

    fix: removed unnecessary route
    Removed the route for getting all courses.

--- course-service/src/routes.ts
+++ course-service/src/routes.ts
@@ -4,7 +4,6 @@ import { controllers } from "./controllers";
 export const router = Router();
 
 router.post("/", controllers.createCourse);
-router.get("/", controllers.getAllCourses);
 router.get("/:id", controllers.getSingleCourse);
 router.patch("/:id/price", controllers.updateCoursePrice);
 router.patch("/:id/reserveSeat", controllers.reserveSeat);
